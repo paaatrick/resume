5f18316d44e3f5be9d930364e6687624fa48b828
Patrick Kalita <patrick.kalita@gmail.com>
2020-12-09 16:25:05 -0800

    fix padding around PDF link on small screens

--- theme/src/components/style/DownloadLink.js
+++ theme/src/components/style/DownloadLink.js
@@ -4,6 +4,11 @@ const DownloadLink = styled.div`
   max-width: ${({theme}) => theme.maxWidth};
   margin: 0 auto ${({theme}) => theme.space(1)} auto;
   text-align: right;
+  padding-right: ${({theme}) => theme.space(1)};
+
+  @media ${({theme}) => theme.breakpoint.jumbo} {
+    padding-right: 0;
+  }
 
   @media print {
     display: none;
